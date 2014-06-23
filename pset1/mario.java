2d6188000e122c52e997751ece02e68fce411246
Sam Hatfield <samuel.e.hatfield@gmail.com>
2014-06-22 22:55:15 -0500

    Started on Mario. We will see how this goes.

---
+++ pset1/mario.java
@@ -0,0 +1,11 @@
+// put imports here
+
+public class Mario
+{
+    public Mario(height)
+    {
+        private String sp = " ";
+        private String br = "#";
+        private String nl = "\n";
+    }
+}
